071d118f3d1ecc277403de7d904df1d0cc585f89
Athira Sanal <athira.physics@gmail.com>
2022-12-05 12:04:01 -0500

    Added start class to the start button on the HTML file
    Added Javascript that makes the start and end game button glow and toggle in and out of visbility.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const allButtons = document.querySelectorAll(".btn")
+const gameButton = document.querySelectorAll(".game_btn")
 
+
+// These lines adds sounds that will later be used.
 const blue = new Audio('sounds/blue.mp3')
 const green = new Audio('sounds/green.mp3')
 const yellow = new Audio('sounds/yellow.mp3')
 const magenta = new Audio('sounds/magenta.mp3')
 
 
-
+// This section adds sounds and visual affects to the game buttons.
 allButtons.forEach(element => { 
     element.addEventListener('click',function(event){
         let color = event.target.classList[0];
@@ -20,4 +23,15 @@ allButtons.forEach(element => {
     })
 });
 
-console.log(colorsAll)
+// This section makes the start and end game button glow and toggle inov visbility. 
+gameButton.forEach(element =>{
+    element.addEventListener('click',function(event){
+        event.target.classList.add("pressed");
+        setTimeout(() => {
+            gameButton.forEach(element =>
+                element.classList.toggle("hidden"));
+                event.target.classList.remove("pressed");  
+        }
+        , 100);
+    });  
+});
